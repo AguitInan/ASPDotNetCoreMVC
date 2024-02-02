eca9efe256c594edd33c7d5fcd48c1eb29981f47
unknown <aguit.inan@gmail.com>
2024-02-02 23:41:49 +0100

    feat: update Tp01

---
+++ Tp01/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tp01.Models;
+
+namespace Tp01.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+
+
+    }
+}
