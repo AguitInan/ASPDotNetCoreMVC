764b2dfb5abeede761e886fa5fa68adac23ff734
unknown <aguit.inan@gmail.com>
2024-02-02 23:43:52 +0100

    feat: update Tp01

--- Tp01/Controllers/TodoItemController.cs
+++ Tp01/Controllers/TodoItemController.cs
@@ -15,7 +15,37 @@ namespace Tp01.Controllers
             _todoItemRepository = todoItemRepository;
         }
 
+        // Route => /TodoItem
+        public IActionResult Index()
+        {
+            return View(_todoItemRepository.GetAll());
+        }
 
+        // Route =>  TodoItemController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View(_todoItemRepository.GetById(id));
+        }
+
+        // Route => TodoItemController/CreateRandom
+        //public IActionResult CreateRandom()
+        //{
+        //    TodoItem? an = new TodoItem()
+        //    {
+        //        FirstName = RandomString("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 5),
+        //        Age = new Random().Next(40),
+        //        Species = RandomString("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 5).ToLower(),
+        //    };
+
+        //    //_fakeAnimalDb.Add(an);
+
+        //    //_dbContext.Animals.Add(an);
+        //    //_dbContext.SaveChanges();
+
+        //    _todoItemRepository.Add(an);
+
+        //    return RedirectToAction(nameof(Index));
+        //}
 
 
 
