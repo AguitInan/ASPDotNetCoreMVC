12113e46cfbe70a1fa00676ff4867eb1c4aa0604
unknown <aguit.inan@gmail.com>
2024-02-02 23:43:39 +0100

    feat: update Tp01

---
+++ Tp01/Controllers/TodoItemController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tp01.Models;
+using Tp01.Data;
+using Tp01.Repositories;
+
+
+namespace Tp01.Controllers
+{
+    public class TodoItemController : Controller
+    {
+        private readonly IRepository<TodoItem> _todoItemRepository;
+
+        public TodoItemController(IRepository<TodoItem> todoItemRepository)
+        {
+            _todoItemRepository = todoItemRepository;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
