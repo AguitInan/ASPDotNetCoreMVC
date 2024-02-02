00ebcb3d4c5faa8d26c0c5a9c15c7394452b6061
unknown <aguit.inan@gmail.com>
2024-02-02 23:39:45 +0100

    add Tp01

---
+++ Tp01/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tp01.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        [Display(Name = "Titre de la tâche")]
+        public string Title { get; set; }
+        [Display(Name = "Description de la tâche")]
+        public string Description { get; set; }
+        [Display(Name = "Statut de la tâche")]
+        public bool IsCompleted { get; set; }
+    }
+}
