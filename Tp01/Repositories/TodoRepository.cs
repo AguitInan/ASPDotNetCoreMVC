e98266357c744c4fe6a6f8f20554c656d0a75b27
unknown <aguit.inan@gmail.com>
2024-02-02 23:47:24 +0100

    feat: update Tp01

---
+++ Tp01/Repositories/TodoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using Tp01.Data;
+using Tp01.Models;
+
+namespace Tp01.Repositories
+{
+    public class TodoRepository : IRepository<TodoItem>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public TodoRepository(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
