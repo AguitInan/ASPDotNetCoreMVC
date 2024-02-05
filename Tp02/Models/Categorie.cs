d96af8443ef58b5ea6bafdc99b638b90f6452cf1
unknown <aguit.inan@gmail.com>
2024-02-05 23:11:30 +0100

    feat: add Tp02

---
+++ Tp02/Models/Categorie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Tp02.Models;
+
+namespace Tp02.Models
+{
+    [Table("categorie")]
+    public class Categorie
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("nom")]
+        public string Nom { get; set; }
+        public List<Produit> Produits { get; set; }
+    }
+}
