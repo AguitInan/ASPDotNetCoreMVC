ed452a831071645da23fcf02db60766251408370
unknown <aguit.inan@gmail.com>
2024-02-05 23:17:09 +0100

    feat: update Tp02

---
+++ Tp02/Repositories/ProduitRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using Tp02.Data;
+using Tp02.Models;
+
+namespace Tp02.Repositories
+{
+    public class ProduitRepository : IRepository<Produit>
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ProduitRepository(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+    }
+}
