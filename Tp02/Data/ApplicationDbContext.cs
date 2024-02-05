8cff46723180ec9e6cacdae38e11af878bf19134
unknown <aguit.inan@gmail.com>
2024-02-05 23:13:42 +0100

    feat: update Tp02

--- Tp02/Data/ApplicationDbContext.cs
+++ Tp02/Data/ApplicationDbContext.cs
@@ -9,5 +9,10 @@ namespace Tp02.Data
         public DbSet<Categorie> Categories { get; set; }
         public DbSet<Produit> Produits { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\demo01ado;Database=caisse_enregistreuse;Trusted_Connection=True;");
+        }
+        
     }
 }
