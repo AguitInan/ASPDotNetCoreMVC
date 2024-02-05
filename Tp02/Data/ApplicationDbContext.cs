d709b6ff80b9d802bb5ee80571aaebb1964d22bf
unknown <aguit.inan@gmail.com>
2024-02-05 23:13:18 +0100

    feat: update Tp02

---
+++ Tp02/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tp02.Models;
+
+namespace Tp02.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Categorie> Categories { get; set; }
+        public DbSet<Produit> Produits { get; set; }
+
+    }
+}
