dbdef0e4e2c4416bff06d634175ba8eee2579421
unknown <aguit.inan@gmail.com>
2024-02-05 23:12:14 +0100

    feat: update Tp02

---
+++ Tp02/Models/Produit.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Tp02.Models;
+
+namespace Tp02.Models
+{
+    [Table("produit")]
+    public class Produit
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("nom")]
+        [Display(Name = "Nom du produit")]
+        public string Nom { get; set; }
+        [Column("description")]
+        [Display(Name = "Description du produit")]
+        public string Description { get; set; }
+        [Column("prix")]
+        [Display(Name = "Prix du produit")]
+        public decimal Prix { get; set; }
+        [Column("quantite")]
+        [Display(Name = "Quantité")]
+
+        public int Quantite { get; set; }
+        [Column("image_url")]
+
+        [Display(Name = "Image du produit")]
+        public string ImageUrl { get; set; }
+        [Column("categorie_id")]
+        [Display(Name = "CatégorieId du produit")]
+        public int CategorieId { get; set; }
+
+        public Categorie Categories { get; set; }
+    }
+}
