03994331d5edf1d3c2ffdc709a320e137acc1bcb
unknown <aguit.inan@gmail.com>
2024-02-07 22:29:25 +0100

    feat: update Tp02

---
+++ Tp02/Controllers/ProduitController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tp02.Models;
+using Tp02.Repositories;
+
+namespace Tp02.Controllers
+{
+    public class ProduitController : Controller
+    {
+        private readonly IRepository<Produit> _repository;
+
+        public ProduitController(IRepository<Produit> repositoryProduit)
+        {
+            _repository = repositoryProduit;
+        }
+    }
+}
